234a0d13884cbfa4e8387eec2783898caf44d759
Eli Skeggs <skeggse@users.noreply.github.com>
2020-07-07 10:27:04 -0700

    feat: initial release (#1)
    Co-authored-by: Trey Tacon <ttacon@gmail.com>

---
+++ fragment_reader_test.go
@@ -0,0 +1,119 @@
+package uploader
+
+import (
+	"io/ioutil"
+	"testing"
+
+	td "github.com/maxatome/go-testdeep/td"
+)
+
+var (
+	alphabet = "abcdefghijklmnopqrstuvwxyz"
+)
+
+func strOfLen(l int) string {
+	v := ""
+	for len(v) < l {
+		v += alphabet
+	}
+	return v[:l]
+}
+
+func readFragments(blobs [][]byte, delim []byte) ([]byte, error) {
+	return ioutil.ReadAll(newFragmentReader(blobs, delim))
+}
+
+func TestReader(t *testing.T) {
+	blobs := [][]byte{
+		[]byte("hello"),
+		[]byte("world"),
+	}
+
+	blob, err := ioutil.ReadAll(newFragmentReader(blobs, []byte{}))
+	if td.CmpNoError(t, err) {
+		td.Cmp(t, blob, []byte("helloworld"))
+	}
+}
+
+func TestReaderDelimiter(t *testing.T) {
+	blobs := [][]byte{
+		[]byte("{}"),
+		[]byte("hflkjlksjdf"),
+	}
+
+	blob, err := readFragments(blobs, []byte("\n"))
+	if td.CmpNoError(t, err) {
+		td.Cmp(t, blob, []byte("{}\nhflkjlksjdf\n"))
+	}
+}
+
+func TestDefaultBoundary(t *testing.T) {
+	// ReadAll passes a 512-byte blob to Read by default.
+	for l := 508; l < 520; l++ {
+		s := strOfLen(l)
+		blobs := [][]byte{
+			[]byte(s),
+			[]byte("asdf"),
+		}
+		td.Cmp(t, len(s), l)
+
+		blob, err := readFragments(blobs, []byte("\n"))
+		if td.CmpNoError(t, err) {
+			td.Cmp(t, blob, []byte(s+"\nasdf\n"))
+		}
+	}
+}
+
+func TestEmptyFragments(t *testing.T) {
+	blobs := [][]byte{
+		[]byte{},
+		[]byte{},
+		[]byte("a"),
+		[]byte{},
+	}
+
+	blob, err := readFragments(blobs, []byte("."))
+	if td.CmpNoError(t, err) {
+		td.Cmp(t, blob, []byte("..a.."))
+	}
+}
+
+func TestNilCases(t *testing.T) {
+	blob, err := readFragments(nil, nil)
+	if td.CmpNoError(t, err) {
+		td.Cmp(t, blob, []byte{})
+	}
+
+	blobs := [][]byte{}
+
+	blob, err = readFragments(blobs, nil)
+	if td.CmpNoError(t, err) {
+		td.Cmp(t, blob, []byte{})
+	}
+
+	blobs = [][]byte{
+		[]byte{},
+	}
+
+	blob, err = readFragments(blobs, nil)
+	if td.CmpNoError(t, err) {
+		td.Cmp(t, blob, []byte{})
+	}
+
+	blob, err = readFragments(blobs, []byte("."))
+	if td.CmpNoError(t, err) {
+		td.Cmp(t, blob, []byte("."))
+	}
+
+	blobs = [][]byte{
+		[]byte{},
+		[]byte{},
+		[]byte("a"),
+		[]byte{},
+	}
+
+	blob, err = readFragments(blobs, nil)
+	if td.CmpNoError(t, err) {
+		td.Cmp(t, blob, []byte("a"))
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/mixmaxhq/go-s3-uploader
+
+go 1.14
+
+require (
+	github.com/aws/aws-sdk-go v1.33.0
+	github.com/maxatome/go-testdeep v1.6.0
+)
